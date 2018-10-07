87d63201b14710403e135b75914f32f8f26d4008
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2018-10-07 12:08:20 +0200

    Use ModiaMath 0.2.4 and adapt two test models to the slightly changed result Dict introduced by ModiaMath 0.2.4.

--- test/test_DynamicExamples.jl
+++ test/test_DynamicExamples.jl
@@ -27,8 +27,8 @@ import .Simulate_DoublePendulum
 @testset "Modia3D: Simulate_Pendulum.jl" begin
    pendulum = Modia3D.SimulationModel( Simulate_Pendulum.Pendulum(Lx=1.6, m=0.5) )
    result = ModiaMath.simulate!(pendulum, stopTime=5.0, tolerance=1e-8, log=false)
-   phi = result.series[Symbol("rev.phi")]
-   w   = result.series[Symbol("rev.w")]
+   phi = result.series["rev.phi"]
+   w   = result.series["rev.w"]
 
    @test isapprox(phi[end], -2.61422; atol=1e-5 )
    @test isapprox(w[end]  , -3.51319; atol=1e-5 )
@@ -38,10 +38,10 @@ end
 @testset "Modia3D: Simulate_DoublePendulum.jl" begin
    doublePendulum = Modia3D.SimulationModel( Simulate_DoublePendulum.DoublePendulum(Lx=1.0, m=1.0) )
    result = ModiaMath.simulate!(doublePendulum, stopTime=5.0, tolerance=1e-8, log=false)
-   phi1 = result.series[Symbol("rev1.phi")]
-   phi2 = result.series[Symbol("rev2.phi")]
-   w1   = result.series[Symbol("rev1.w")]
-   w2   = result.series[Symbol("rev2.w")]
+   phi1 = result.series["rev1.phi"]
+   phi2 = result.series["rev2.phi"]
+   w1   = result.series["rev1.w"]
+   w2   = result.series["rev2.w"]
 
    @test isapprox(phi1[end], -0.211087; atol=1e-3 )
    @test isapprox(phi2[end],  0.458381; atol=1e-3 )
