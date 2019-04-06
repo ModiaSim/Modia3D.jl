4d79ac4e572c67d3ab81af74ac202f18153b7490
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2019-04-06 08:04:13 +0200

    Date updated

--- src/Modia3D.jl
+++ src/Modia3D.jl
@@ -5,7 +5,7 @@ module Modia3D
 
 const path = dirname(dirname(@__FILE__))   # Absolute path of package directory
 const Version = "0.3.0"
-const Date = "2019-04-05"
+const Date = "2019-04-06"
 
 println("\nImporting Modia3D Version $Version ($Date)")
 
