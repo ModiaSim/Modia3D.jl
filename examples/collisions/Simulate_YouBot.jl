569c3aba186bbc9e9341ea567ca6478769b13f2b
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2019-07-17 06:52:23 +0200

    Simulate_YouBot example: minor cleanup

--- examples/collisions/Simulate_YouBot.jl
+++ examples/collisions/Simulate_YouBot.jl
@@ -4,7 +4,7 @@ using  Modia3D
 import Modia3D.ModiaMath
 using  Modia3D.ModiaMath.Unitful
 
-vmat1 = Modia3D.Material(color="LightBlue" , transparency=0.0)    # material of SolidFileMesh
+vmat1 = Modia3D.Material(color="LightBlue" , transparency=0.8)    # material of SolidFileMesh
 
 base_frame_obj           = joinpath(Modia3D.path, "objects/robot_KUKA_YouBot/base_frame.obj")
 plate_obj                = joinpath(Modia3D.path, "objects/robot_KUKA_YouBot/plate.obj")
@@ -35,7 +35,7 @@ end
 
 
 # Controller
-@forceElement P_PI_Controller(; k1=1.0, k2=1.0, T2=0.01, freqHz=0.5, A=1.0) begin
+@forceElement P_PI_Controller(; k1=1.0, k2=10.0, T2=0.01, freqHz=0.05, A=1.0) begin
     PI_x    = ModiaMath.RealScalar("PI_x"   , start=0.0, info="State of PI controller", numericType=ModiaMath.XD_EXP)
     PI_derx = ModiaMath.RealScalar("PI_derx", start=0.0, info="= der(PI_x)"           , numericType=ModiaMath.DER_XD_EXP, integral=PI_x)
     sine_y  = ModiaMath.RealScalar("sine_y" , start=0.0, info="= sin(2*pi*f*time)"    , numericType=ModiaMath.WR)
@@ -52,32 +52,36 @@ function Modia3D.computeTorque(c::P_PI_Controller, sim::ModiaMath.SimulationStat
 end
 
 
-@signal Sine(;A=1.5, freqHz = 0.5) begin
+@signal Sine(;A=1.5, freqHz = 0.5, offset=0.0) begin
    y = ModiaMath.RealScalar(causality=ModiaMath.Output, numericType=ModiaMath.WR)
 end
 function Modia3D.computeSignal(signal::Sine, sim::ModiaMath.SimulationState)
-    signal.y.value  = signal.A*sin(2*pi*signal.freqHz*sim.time)
+    signal.y.value  = signal.offset + signal.A*sin(2*pi*signal.freqHz*sim.time)
 end
 
 
-@assembly Link(frame_a; axis=3, R_a_rev=ModiaMath.NullRotation, r_rev_b=[0.0, 0.0, 0.0], m=0.0, fileMesh="",
-                d=0.0, k1=10.0, k2=10.0, T2=0.01, freqHz=0.1, A=1.0) begin
-    rev_a   = Object3D(frame_a, R=R_a_rev)
-    rev_b   = Object3D(Solid(SolidFileMesh(fileMesh),m,vmat1), visualizeFrame=false)
-    rev     = Revolute(rev_a, rev_b)
-    frame_b = Object3D(rev_b, r=r_rev_b)
+@assembly Link(frame_a; axis=3, fileMesh="", m=0.0, r_rev_b=[0.0, 0.0, 0.0], R_a_rev=ModiaMath.NullRotation,
+               d=0.0, k1=1.0, k2=10.0, T2=0.01, freqHz=0.1, A=0.1, offset=0.0) begin
+    rev_a   = Object3D(frame_a, R=R_a_rev, visualizeFrame=false)
+    rev_b   = Object3D(Solid(SolidFileMesh(fileMesh),m,vmat1))
+    rev     = Revolute(rev_a, rev_b, phi_start=offset)
+    frame_b = Object3D(rev_b, r=r_rev_b, visualizeFrame=false)
 
+#=
     # Kinematic movement of joint
-    sig         = Sine(A=A,freqHz=freqHz)
+    sig         = Sine(A=A,freqHz=freqHz,offset=offset)
     sig_adaptor = Modia3D.SignalToFlangeAngle(sig.y)
     Modia3D.connect(sig_adaptor, rev)
+=#
 
 #=
     # Damping in the joint
     damper         = Damper(d=d)
     damper_adaptor = Modia3D.AdaptorForceElementToFlange(w=damper.w, tau=damper.tau)
     Modia3D.connect(damper_adaptor, rev)
+=#
 
+#=
     # P-PI controller in the joint
     controller         = P_PI_Controller(k1=k1, k2=k2, T2=T2, freqHz=freqHz, A=A)
     controller_adaptor = Modia3D.AdaptorForceElementToFlange(phi=controller.phi, w=controller.w, tau=controller.tau)
@@ -106,19 +110,22 @@ end
     base            = Base(world)
     arm_base_frame  = Object3D(base.base_frame, Solid(SolidFileMesh(arm_base_frame_obj), 0.961, vmat1), r=[0.143, 0.0, 0.046], visualizeFrame=false)
     armBase_b       = Object3D(arm_base_frame, r=[0.024, 0, 0.115])
-    link1   = Link(armBase_b, R_a_rev = ModiaMath.rot1(180u"°"), r_rev_b=[0.033,0,0], m=1.390, fileMesh=arm_joint_1_obj)
-    link2   = Link(link1.frame_b, R_a_rev = ModiaMath.rot123(90u"°", 0.0,-90u"°"), r_rev_b=[0.155,0,0], m=1.318, fileMesh=arm_joint_2_obj)
-    link3   = Link(link2.frame_b, R_a_rev = ModiaMath.rot3(-90u"°"), r_rev_b=[0,0.135,0], m=0.821, fileMesh=arm_joint_3_obj)
-    link4   = Link(link3.frame_b, r_rev_b=[0,0.11316,0], m=0.769, fileMesh=arm_joint_4_obj)
-    link5   = Link(link4.frame_b, R_a_rev = ModiaMath.rot1(-90u"°"), r_rev_b=[0,0,0.05716], m=0.687, fileMesh=arm_joint_5_obj)
+    link1   = Link(armBase_b    , fileMesh=arm_joint_1_obj, m=1.390, r_rev_b=[0.033,0,0], R_a_rev = ModiaMath.rot1(180u"°"))
+    link2   = Link(link1.frame_b, fileMesh=arm_joint_2_obj, m=1.318, r_rev_b=[0.155,0,0], R_a_rev = ModiaMath.rot123(90u"°", 0.0,-90u"°"))
+    link3   = Link(link2.frame_b, fileMesh=arm_joint_3_obj, m=0.821, r_rev_b=[0,0.135,0], R_a_rev = ModiaMath.rot3(-90u"°"), offset=pi/2, A=0.0)
+    link4   = Link(link3.frame_b, fileMesh=arm_joint_4_obj, m=0.769, r_rev_b=[0,0.11316,0])
+    link5   = Link(link4.frame_b, fileMesh=arm_joint_5_obj, m=0.687, r_rev_b=[0,0,0.05716], R_a_rev = ModiaMath.rot1(-90u"°"))
     gripper = Gripper(link5.frame_b)
 end
 
 gravField = UniformGravityField(g=9.81, n=[0,0,-1])
-youBot    = YouBot(sceneOptions=SceneOptions(gravityField=gravField, visualizeFrames=false,
-                                             defaultFrameLength=0.2, enableContactDetection=false))
+youBot    = YouBot(sceneOptions=SceneOptions(gravityField=gravField, visualizeFrames=true,
+                                             defaultFrameLength=0.1, enableContactDetection=false))
+
+println("... arm_joint_1: massProperties = ", youBot.link1.rev_b.data.massProperties)
 
 #Modia3D.visualizeAssembly!(youBot)
-model  = Modia3D.SimulationModel( youBot, analysis=ModiaMath.KinematicAnalysis )
-result = ModiaMath.simulate!(model, stopTime=5.0, log=true, tolerance=1e-4)
+#model  = Modia3D.SimulationModel( youBot, analysis=ModiaMath.KinematicAnalysis )
+model  = Modia3D.SimulationModel( youBot )
+result = ModiaMath.simulate!(model, stopTime=0.07, log=true, tolerance=1e-4)
 end
