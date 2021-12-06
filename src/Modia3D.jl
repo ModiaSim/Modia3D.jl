429a4576e2587fb0d3c20808c9cf7b7bf3e4530f
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2021-12-06 19:35:18 +0100

    Missing String character corrected

--- src/Modia3D.jl
+++ src/Modia3D.jl
@@ -5,7 +5,7 @@ module Modia3D
 
 const path = dirname(dirname(@__FILE__))   # Absolute path of package directory
 const Version = "0.6.0"
-const Date = "2021-12-06
+const Date = "2021-12-06"
 
 # println("\nImporting Modia3D Version $Version ($Date)")
 
