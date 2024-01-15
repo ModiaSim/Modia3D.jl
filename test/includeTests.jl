39a31c98233f4bfb1cb3c67122ce50564c0f7939
Gerhard Hippmann <gerhard.hippmann@dlr.de>
2024-01-15 11:37:23 +0100

    Update workflow tests to julia 10.0.0

--- test/includeTests.jl
+++ test/includeTests.jl
@@ -14,7 +14,7 @@ Test.@testset "Basic" begin
     include(joinpath("Basic", "AllShapes.jl"))
     include(joinpath("Basic", "PendulumWithBar1.jl"))
     include(joinpath("Basic", "PendulumWithBar2.jl"))
-    Test.@test_broken include(joinpath("Basic", "PendulumWithBar3.jl"))  # error when called from here, works when called from REPL
+    include(joinpath("Basic", "PendulumWithBar3.jl"))
     include(joinpath("Basic", "PendulumWithDamper.jl"))
     include(joinpath("Basic", "PendulumWithFix.jl"))
     include(joinpath("Basic", "PendulumWithParameterizedDamper.jl"))
