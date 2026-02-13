a418027baf49dff4666553dbbf867a9c4aaf06bc
Andrea Neumayr <andrea.neumayr@dlr.de>
2026-02-13 12:50:02 +0100

    update version number

--- src/Modia3D.jl
+++ src/Modia3D.jl
@@ -4,8 +4,8 @@
 module Modia3D
 
 const path = dirname(dirname(@__FILE__))   # Absolute path of package directory
-const Version = "0.12.2"
-const Date = "2023-11-23"
+const Version = "0.12.3"
+const Date = "2026-02-13"
 
 println("\nImporting Modia3D Version $Version ($Date)")
 
